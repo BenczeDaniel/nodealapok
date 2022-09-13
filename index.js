fbe20101a0b237eb2dd5c7f50044a4c5165a9e7a
Your Name <you@example.com>
2022-09-13 13:20:20 +0200

    mysql

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-import express, { request } from 'express';
-import { diakok } from './adatok.js';
+import { config } from "./dbconfig.js";
+import express from "express";
+import mysql from 'mysql';
 
 const app = express();
 app.use(express.json())
+const db=mysql.createConnection(config)
 
-app.get('/',(request,response) => {
-   // response.send('Saját szerverünk küldi ezt az üzenetet')
-   response.send(diakok)
+app.get('/',(request, response) => {
+    db.query('SELECT id,author FROM books GROUP BY author ORDER BY author',(err,result)=>{
+        if(err)
+            console.log(err)
+        else
+            response.send(result)
+    
+    })
+
 })
 
 
-app.get('/:id',(request,response) => {
-    const {id} = request.params//a u url-ben érkező paraméterek elérése
-    const filteredArr= diakok.filter(obj=>obj.id==id)
-    response.send(filteredArr)})
+app.listen(5000,() => console.log('server listening on port 5000....'))
 
-
-    app.post('/',(request,response) => {
-        const {id,nev,osz}=request.body
-        diakok.push({id:id,nev:nev,osz:osz})
-        response.send(diakok)
-    })
-    app.get('*',(request,response) => {
-        response.status(404).send('<h1>Az oldal nem létezik....</h1>')})
-
-app.listen(5000,() => console.log('Fut a szerver az 5000-res porton!!'))
