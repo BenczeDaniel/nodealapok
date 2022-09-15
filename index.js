194b882edefc03269313ae00e51c526ac61f6f73
Your Name <you@example.com>
2022-09-15 13:12:55 +0200

    2022.09.15

--- index.js
+++ index.js
@@ -18,5 +18,47 @@ app.get('/',(request, response) => {
 })
 
 
+app.get('/szerzo/:author',(request, response) => {
+    const {author} = request.params
+
+    db.query('SELECT title FROM books WHERE author=? ORDER BY title',[author],(err,result)=>{
+        if(err)
+            console.log(err)
+        else
+            response.send(result)
+    
+    })
+
+})
+
+app.get('/categ/:category/:year',(request, response) => {
+    const {category,year} = request.params
+
+    db.query('SELECT author,title,year FROM books WHERE year>=? AND category=?',[year,category],(err,result)=>{
+        if(err)
+            console.log(err)
+        else
+            response.send(result)
+    
+    })
+
+})
+
+app.get('/id/:id',(request, response) => {
+    const {id} = request.params
+
+    db.query('SELECT author,title,year,category FROM books WHERE id=? ORDER BY title',[id],(err,result)=>{
+        if(err)
+            console.log(err)
+        else
+            response.send(result)
+    
+    })
+
+})
+
+
+
+
 app.listen(5000,() => console.log('server listening on port 5000....'))
 
