9b86c2742b09893371866e5b0cdd312c5d7e905b
Your Name <you@example.com>
2022-09-16 09:25:02 +0200

    2022.09.16

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { config } from "./dbconfig.js";
-import express from "express";
+import express, { request } from "express";
 import mysql from 'mysql';
 
 const app = express();
@@ -31,7 +31,7 @@ app.get('/szerzo/:author',(request, response) => {
 
 })
 
-app.get('/categ/:category/:year',(request, response) => {
+app.get('/categ/:category/year/:year',(request, response) => {
     const {category,year} = request.params
 
     db.query('SELECT author,title,year FROM books WHERE year>=? AND category=?',[year,category],(err,result)=>{
@@ -56,6 +56,75 @@ app.get('/id/:id',(request, response) => {
     })
 
 })
+
+app.put('/:id/:year',(request, response)=>{
+    const {id,year} = request.params
+    db.query('UPDATE books SET year=? WHERE id=? ',[year,id],(err,result)=>{
+
+        if(err)
+            console.log(err)
+        if(result.affectedRows==1)
+        response.send({message:"Sikeres adatmódosítás!"})
+        else
+        response.send({message:"Nem sikeres adatmódosítás!"})
+
+    })
+
+})
+
+
+app.put('/',(request, response)=>{
+    const {id,category,year} = request.params
+    db.query('UPDATE books SET category=? , year=? WHERE id=? ',[category,year,id],(err,result)=>{
+
+        if(err)
+            console.log(err)
+        if(result.affectedRows==1)
+        response.send({message:`Sikeres adatmódosítás! id:${result.insertId},`})
+        else
+        response.send({message:"Nem sikeres adatmódosítás!"})
+
+    })
+
+})
+
+
+app.post('/',(request, response)=>{
+    const {category,year,title,author} = request.params
+    db.query('Insert into books values(null,?,?,?,?)',[author,title,year,category],(err,result)=>{
+
+        if(err)
+            console.log(err)
+        if(result.insertId)
+        response.send({message:"Sikeres adatmódosítás!"})
+        else
+        response.send({message:"Nem sikeres adatmódosítás!"})
+
+    })
+
+})
+
+
+
+app.delete('/:id',(request, response)=>{
+    const {id} = request.params
+    db.query('delete from books where id=?',[id],(err,result)=>{
+
+        if(err)
+            console.log(err)
+        if(result.insertId)
+        response.send({message:"Sikeres adatmódosítás!"})
+        else
+        response.send({message:"Nem sikeres adat törlés"})
+
+    })
+
+})
+
+//a felhasználó látni szeretné hogy a kiválasztott kategoriában milyen könyvek vannak(szerző,cím)
+
+
+
 
 
 
